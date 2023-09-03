6faa27d2d5143fe478ccaa06ef707b6fe45f3f50
Gustavo Batista <gustavo.batista@bluecore.it>
2023-09-03 16:38:54 -0300

    feat: :sparkles: add header component and dummy datas

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 
 import '@/styles/globals.css'
+import Header from '@/components/header'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -23,6 +24,8 @@ export default function RootLayout({
         <div className="absolute right-[11rem] top-[-6rem] -z-10 h-[31.5rem] w-[31.5rem] rounded-full bg-gradient-to-r from-[#2d74f8] via-[#000000] to-[#2d74f8] opacity-70 blur-[10rem] sm:w-[68.75rem]" />
         <div className="absolute left-[-35rem] top-[-1rem] -z-10 h-[31.5rem] w-[50rem] rounded-full bg-gradient-to-r from-[#000000] via-[#2d74f8] to-[#000000] opacity-70 blur-[20rem] sm:w-[68.75rem] md:left-[-33rem] lg:left-[-28rem] xl:left-[-15rem] 2xl:left-[-5rem]" />
 
+        {/* Header */}
+        <Header />
         {/* Content */}
         {children}
       </body>
